65a5f32dda1c26875424c71bfc530745c048432f
CiroGN <ciroguilhermenass@gmail.com>
2025-06-07 19:50:54 -0300

    termino aula paper

--- 1Semestre/DesMov/ReNat/Paper/app/_layout.tsx
+++ 1Semestre/DesMov/ReNat/Paper/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
+import CustomLightTheme from './theme'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <PaperProvider theme={CustomLightTheme}>
+      <Stack>
+        <Stack.Screen name='index' options={{title:'Paper'}}/>
+      </Stack>
+    </PaperProvider>
+  );
 }
--- 1Semestre/DesMov/ReNat/Paper/app/index.tsx
+++ 1Semestre/DesMov/ReNat/Paper/app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { linkTo } from "expo-router/build/global-state/routing";
+import { useState } from "react";
+import { View } from "react-native";
+import { Text, TextInput, Button, Snackbar } from 'react-native-paper';
 
 export default function Index() {
+  const [nomeusuario, setNomeUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [visible, setVisible] = useState(false);
+  const onToggleSnackBar = () => setVisible(!visible);
+  const onDismissSnackBar = () => setVisible(false);
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{flex: 1, gap: 25, marginLeft: 50, marginRight: 50, marginTop: 20}}>   
+        <Text style={{textAlign: "center"}} variant="displayMedium">Login</Text>
+        <TextInput
+          label="Usuário"
+          value={nomeusuario}
+          onChangeText={texto => setNomeUsuario(texto)}
+        />
+        <TextInput
+          label="Senha"
+          value={senha}
+          onChangeText={text => setSenha(text)}
+          secureTextEntry={true}
+        />
+        <Button icon="lock" mode="contained" onPress={onToggleSnackBar}>
+          Acessar
+        </Button>
+
+        <Snackbar
+          visible={visible}
+          onDismiss={onDismissSnackBar}
+          action={{
+            label: 'Fechar',
+            onPress: () => {
+              onToggleSnackBar
+            },
+          }}>
+          Aguarde. Processando login...
+        </Snackbar>
+
+        <Button onPress={()=>{linkTo:"/card"}}>
+          Card
+        </Button>
+
     </View>
   );
 }
