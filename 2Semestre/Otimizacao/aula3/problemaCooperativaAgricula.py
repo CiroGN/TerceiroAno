da5f081a854a4ba3ca58478eab4b3f4631791cd8
CiroGN <ciroguilhermenass@gmail.com>
2025-09-05 18:12:53 -0300

    aula cooperativa

--- 2Semestre/Otimizacao/aula3/problemaCooperativaAgricula.py
+++ 2Semestre/Otimizacao/aula3/problemaCooperativaAgricula.py
@@ -1,68 +1,101 @@
 import pyomo.environ as Pyo
+from pyomo.opt import SolverStatus, TerminationCondition
 
+#Apenas no Windows
 EXEC_PATH = "C:\glpk-4.65\w64\glpsol.exe"
 
-#Definição dos COnjuntos
-C = {'Milho', 'Feijão', 'Arroz'} # i in C
-F = {'Fazenda 1', 'Fazenda 2', 'Fazenda 3'} # j in F
+#Definindo os conjuntos
+C = ['Milho', 'Arroz', 'Feijao'] # i
+F = ['Fazenda 1', 'Fazenda 2', 'Fazenda 3'] # j
 
-# Parâmetros do Problema
-L_i = {('Milho'):5000, ('Arroz'):4000, ('Feijão'):1800} #Lucro em R$/Acre
-A_j = {('Fazenda 1'):400, ('Fazenda 2'):650, ('Fazenda 3'):350} # Área disponível em acres
-W_j = {('Fazenda 1'):1800, ('Fazenda 2'):2200, ('Fazenda 3'):950}
-w_i = {('Milho'):5.5, ('Arroz'):4.0, ('Feijão'):3.5} # Consumo de água por cultura em litros/acre
-M_i = {('Milho'):660, ('Arroz'):880, ('Feijão'):440} # Área máxima por cultura, em acres
+#Definindo os Parâmetros
+L = {('Milho') : 5000, ('Arroz') : 4000, ('Feijao') : 1900} # Lucro por cultura
+A = {('Fazenda 1') : 400, ('Fazenda 2') : 650, ('Fazenda 3') : 350} # Área disponível em cada fazenda
+alfa = {('Milho') : 5.5, ('Arroz') : 4.0, ('Feijao') : 3.5} # consumo de água por acre cultivado com cada cultura
+W = {('Fazenda 1') : 1800, ('Fazenda 2') : 2201, ('Fazenda 3') : 950} # Água disponível por fazenda
+U = {('Milho') : 660, ('Arroz') : 880, ('Feijao') : 440} # Área máxima por cultura
 
 #Criando o Modelo
 Modelo = Pyo.ConcreteModel()
 
-# [V.D.]
-Modelo.X_ij = Pyo.Var(C, F, within=Pyo.NonNegativeReals) # X_ij >= 0, i in C, j in F
+#Declarando as Variáveis de Decisão
+Modelo.X = Pyo.Var(C, F, within=Pyo.NonNegativeReals) # X_ij >= 0
 
-# [F.O.]
-Modelo.FO = Pyo.Objective(expr = sum( sum(L_i[i]*Modelo.X_ij[i,j] for j in F)for i in C), sense = Pyo.maximize)
+#Modelando a Função Objetivo
+Modelo.FO = Pyo.Objective(expr = sum(L[i] * sum(Modelo.X[i,j] for j in F) for i in C), sense=Pyo.maximize)
 
-# Restrição de Área por Fazendas
+#Modelando as Restrições
+
+# 1 - Área por Fazenda
 Modelo.R_AreaFazenda = Pyo.ConstraintList()
-for j in F:
-    Modelo.R_AreaFazenda.add(expr = sum(Modelo.X_ij[i,j]for i in C) <= A_j[j])
+for j in A:
+    Modelo.R_AreaFazenda.add(expr = sum(Modelo.X[i,j] for i in C) <= A[j])
 
-# Restrição de Água por Fazendas
+# 2 - Água por Fazenda
 Modelo.R_AguaFazenda = Pyo.ConstraintList()
-for j in F:
-    Modelo.R_AguaFazenda.add(expr = sum(w_i[i]*Modelo.X_ij[i,j] for i in C) <=W_j[j])
+for j in W:
+    Modelo.R_AguaFazenda.add(expr = sum(alfa[i] * Modelo.X[i,j] for i in C) <= W[j])
 
-# Restrição de Área por Culturas
-Modelo.R_AreaCulturas = Pyo.ConstraintList()
-for i in C:
-    Modelo.R_AreaCulturas.add(expr = sum(Modelo.X_ij[i,j] for j in F) <= M_i[i])
+# 3 - Área por Cultura
+Modelo.R_AreaCultura = Pyo.ConstraintList()
+for i in U:
+    Modelo.R_AreaCultura.add(expr = sum(Modelo.X[i,j] for j in F) <= U[i])
 
-# Restrição de Proporção de Áreas
+# 4 - Proporção de Áreas por Fazenda
 Modelo.R_ProporcaoAreas = Pyo.ConstraintList()
 u = 0
-for j in F:
+for j in A:
     u += 1
     v = 0
-    for k in F:
+    for k in A:
         v += 1
-        if (v - u) == 1 and (k != j):
-            print(f'v = {v}  -  u = {u}')
-            Modelo.R_ProporcaoAreas.add(expr = ((1/A_j[j])*sum(Modelo.X_ij[i,j] for i in C) -
-                                                (1/A_j[k])*sum(Modelo.X_ij[i,j] for i in C)) == 0)
-
-Otimizador = Pyo.SolverFactory('glpk', executable = EXEC_PATH)
-Resultado = Otimizador.solve(Modelo)
-
-# Imprimindo resultados
-print(f'Status da otimização:', Resultado.solver.status)
-print(f'Critério de Parada:', Resultado.solver.termination_condition)
-
-# Imprimindo a FO Ótima
-print(f'Solução Ótima:', Pyo.value(Modelo.FO))
-
-# Imprimindo a Solução
-for v in Modelo.component_objects(ctype=Pyo.Var):
-    for index in v:
-        print('{0} == {1}'.format(v[index], round(Pyo.value(v[index], 2))))
+        if (v - u) ==  1:
+            Modelo.R_ProporcaoAreas.add(expr = sum(Modelo.X[i,j] for i in C)/A[j] == sum(Modelo.X[i,k] for i in C)/A[k])
 
 Modelo.pprint()
+
+#Setup do Solver
+Solver = Pyo.SolverFactory('glpk', executable=EXEC_PATH)
+
+#Resolvendo o Modelo
+Solucao = Solver.solve(Modelo)
+Pyo.assert_optimal_termination(Solucao)
+
+# Imprimindo a Solução
+if (Solucao.Solver.status == SolverStatus.ok) and (Solucao.Solver.termination_condition == TerminationCondition.optimal):
+    #Solução Ótima Encontrada
+    print() # pula uma linha
+    print('Solucao Otima = $', round(Pyo.value(Modelo.FO),2)) # $ ------,00
+    print() # pula uma linha
+
+    #Imprimindo a solução ótima nas VD's
+    for v in Modelo.component_objects(ctype=Pyo.Var):
+        for index in v:
+            print('{0} == {1}'.format(v[index], round(Pyo.value(v[index]),2)))
+    print() # pula uma linha
+
+    #Imprimindo as Restrições
+    print('Área por Fazenda')
+    f = 0 
+    for j in A:
+        f += 1
+        LHS_Area_Fazenda = sum(Pyo.value(Modelo.X[i,j]) for i in C)
+        print('Fazenda ' + str(f) + ':' + str(round(LHS_Area_Fazenda,2)) + " <= " + str(A[j]) + '  Percentual = ' + str(round(100*LHS_Area_Fazenda/A[j],2)) + "%")
+
+    print() # pula uma linha
+    print('Consumo de Água por Fazenda')
+    f = 0 
+    for j in W:
+        f += 1
+        LHS_Agua_Fazenda = sum(Pyo.value(alfa[i]*Modelo.X[i,j]) for i in C)
+        print('Fazenda ' + str(f) + ':' + str(round(LHS_Agua_Fazenda,2)) + " <= " + str(W[j]) + '  Percentual = ' + str(round(100*LHS_Agua_Fazenda/W[j],2)) + "%")
+
+    print() # pula uma linha
+    print('Área por Cultura')
+    c = 0
+    for i in U:
+        LHS_Area_Cultura = sum(Pyo.value(Modelo.X[i,j]) for j in F)
+        print(str(list(U.keys())[c]) + ' : ' + str(round(LHS_Area_Cultura,2)) + " <= " + str(U[i]))
+        c += 1
+else:
+    print('Deu ruim')
